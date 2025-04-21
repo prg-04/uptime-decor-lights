dcb70729d5929794573121a6f66c78844e0f9837
prg-04 <karanjaevans95@gmail.com>
2025-04-21 08:39:15 +0300

    feat: update CategoryPage to await params and add typing for slug

--- app/(store)/categories/[slug]/page.tsx
+++ app/(store)/categories/[slug]/page.tsx
@@ -5,9 +5,9 @@ import getProductsByCategory from "@/sanity/lib/products/getProductsByCategory";
 export default async function CategoryPage({
   params,
 }: {
-  params: { slug: string };
+  params: Promise<{ slug: string }>;
 }) {
-  const { slug } = params;
+  const { slug } = await params;
   const products = await getProductsByCategory(slug);
   const categories = await getAllCategories();
 
@@ -17,7 +17,7 @@ export default async function CategoryPage({
         <h1 className="text-3xl font-bold mb-6 text-center">
           {slug
             .split("-")
-            .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+            .map((word: string) => word.charAt(0).toUpperCase() + word.slice(1))
             .join(" ")}
         </h1>
         <ProductsView products={products} categories={categories} />
