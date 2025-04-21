9e9ca58b4f59713669d04408c938396e15dfa11e
prg-04 <karanjaevans95@gmail.com>
2025-04-21 09:54:02 +0300

    feat: update SearchPage to await searchParams and improve JSX formatting

--- app/(store)/search/page.tsx
+++ app/(store)/search/page.tsx
@@ -5,7 +5,7 @@ import ProductGrid from "@/components/ProductGrid";
 const SearchPage = async ({
   searchParams,
 }: {
-  searchParams: { query: string };
+  searchParams: Promise<{ query: string }>;
 }) => {
   const { query } = await searchParams;
   const products = await searchProductsByName(query);
@@ -25,14 +25,16 @@ const SearchPage = async ({
     );
   }
 
-  return <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100 p-4">
-    <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-4xl">
-      <h1 className="text-3xl font-bold mb-6 text-center">
-        Search results for: {query}
-      </h1>
-      <ProductGrid products={products} />
+  return (
+    <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100 p-4">
+      <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-4xl">
+        <h1 className="text-3xl font-bold mb-6 text-center">
+          Search results for: {query}
+        </h1>
+        <ProductGrid products={products} />
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default SearchPage;
