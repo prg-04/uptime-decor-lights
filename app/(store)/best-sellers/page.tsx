1446b3a66b2fddc8247b79f70824daabcc922106
prg-04 <karanjaevans95@gmail.com>
2025-04-21 07:49:36 +0300

    feat: rename best sellers page component for clarity and consistency

--- app/(store)/best-sellers/page.tsx
+++ app/(store)/best-sellers/page.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+const BestSellerPage = () => {
+  return <div>BestSellerPage</div>;
+};
 
-export default page
+export default BestSellerPage;
