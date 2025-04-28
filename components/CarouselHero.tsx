e2a901431d5a5f5df6f298c143300fd9088805b3
prg-04 <karanjaevans95@gmail.com>
2025-04-28 17:33:36 +0300

    `Removed heroContent prop from Carousel component in CarouselHero component`

--- components/CarouselHero.tsx
+++ components/CarouselHero.tsx
@@ -1,12 +1,10 @@
-import getHeroSectionContent from "@/sanity/lib/hero/getHeroSectionContent";
 import { Carousel } from "./ui/Carousel";
 // import Carousel from "./Carousel";
 
 const CarouselHero = async () => {
-  const heroContent = await getHeroSectionContent();
   return (
     <div className="relative w-full">
-      <Carousel items={heroContent} />
+      <Carousel />
     </div>
   );
 };
