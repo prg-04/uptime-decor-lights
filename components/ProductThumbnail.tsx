d562c3779c78cc93fdcda81b80af08f46eb33412
prg-04 <karanjaevans95@gmail.com>
2025-04-28 18:42:24 +0300

    `Refactor ProductThumbnail component to use type assertion for image reference`

--- components/ProductThumbnail.tsx
+++ components/ProductThumbnail.tsx
@@ -4,29 +4,22 @@ import Link from "next/link";
 import Image from "next/image";
 import { imageUrl } from "@/lib/imageUrl";
 
-// interface ImageReference {
-//   image?: {
-//     asset?: {
-//       _ref: string;
-//     };
-//   };
-// }
+interface ImageReference {
+  image?: {
+    asset?: {
+      _ref: string;
+    };
+  };
+}
+
 
 const ProductThumbnail = ({ product }: { product: Product }) => {
   const isOutOfStock = product.stock != null && product.stock <= 0;
 
-  // Try to get the image reference from either structure
-  let imageRef = null;
-  
-  // Check for nested image structure from Sanity query
-  if (product.image?.[0]?.image?.asset?._ref) {
-    imageRef = product.image[0].image.asset._ref;
-  }
-  // Check for reference structure from TypeScript types
-  else if (product.image?.[0]?._ref) {
-    imageRef = product.image[0]._ref;
-  }
-  
+  // Use type assertion to access the nested structure
+  const imageRef =
+    product.image?.[0] && (product.image[0] as ImageReference).image?.asset?._ref;
+
   console.log("Product on ProductThumbnail:", imageRef);
 
   return (
