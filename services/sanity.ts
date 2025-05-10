1015c3572ecd6184f9df6dd4cf30f2254638ab47
prg-04 <karanjaevans95@gmail.com>
2025-05-10 16:03:51 +0300

    `Update import paths in sanity.ts to use relative paths`

--- services/sanity.ts
+++ services/sanity.ts
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { sanityClient } from "@/lib/sanityClient"; 
+import { sanityClient } from "lib/sanityClient";
 import { groq } from "next-sanity";
 import { cache } from "react";
-import type { CarouselItem } from "@/types"; // Import CarouselItem type
-import { homeContent as defaultRawSeoItems } from "@/constants/homeContent"; // Import default raw SEO content
+import type { CarouselItem } from "types"; // Import CarouselItem type
+import { homeContent as defaultRawSeoItems } from "constants/homeContent"; // Import default raw SEO content
 
 /**
  * Represents a single image with optional alt text from Sanity.
